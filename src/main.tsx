04b7df74f70628efa65205395490e2248d2b2047
10thousandbands2.0 <10thousandbands2.0@10thousandbands20s-MacBook-Pro.local>
2025-04-21 13:08:48 -0400

    feat: configure ThirdWeb for Solana devnet

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,10 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { ThirdwebProvider } from '@thirdweb-dev/react'
-import { Sepolia } from '@thirdweb-dev/chains'
 
 createRoot(document.getElementById("root")!).render(
-  <ThirdwebProvider
-    activeChain={Sepolia}
+  <ThirdwebProvider 
+    activeChain="solana:devnet"
     clientId={import.meta.env.VITE_THIRDWEB_CLIENT_ID}
   >
     <App />
