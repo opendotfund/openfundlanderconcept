63d6035368ee00ad417989ac3c1037ae395c8d4c
10thousandbands2.0 <10thousandbands2.0@10thousandbands20s-MacBook-Pro.local>
2025-04-21 13:05:49 -0400

    fix: revert to Sepolia chain configuration

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,11 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { ThirdwebProvider } from '@thirdweb-dev/react'
+import { Sepolia } from '@thirdweb-dev/chains'
 
 createRoot(document.getElementById("root")!).render(
   <ThirdwebProvider
-    activeChain="solana"
+    activeChain={Sepolia}
     clientId={import.meta.env.VITE_THIRDWEB_CLIENT_ID}
   >
     <App />
