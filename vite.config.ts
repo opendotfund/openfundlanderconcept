15602b730a613e7ab26c2f95ec7a1e684260b206
10thousandbands2.0 <10thousandbands2.0@10thousandbands20s-MacBook-Pro.local>
2025-04-21 13:18:42 -0400

    Fix favicon and asset paths, update Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'development' ? '/' : '/preview/',
+  base: '/',
   server: {
     host: true,
     port: 3001,
